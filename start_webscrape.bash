1bb0f5a4b8ff2ae526e9900503a0c804eddf6689
Beastan <dhanoop.karunakaran@gmail.com>
2024-11-20 21:58:25 +1100

    changes

--- start_webscrape.bash
+++ start_webscrape.bash
@@ -1,5 +1,8 @@
 #!/bin/bash
 
 
-cd taxinfowebscraping/
-scrapy crawl taxinfo -s JOBDIR=save_spider-1
+#cd taxinfowebscraping/
+#scrapy crawl taxinfo -s JOBDIR=save_spider-1
+
+cd grocery_scraping  
+scrapy crawl coles
